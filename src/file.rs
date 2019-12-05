7feecd617b4051e8e694be77b27d5394cfea6790
Aslam Ahammed <aslamplr@gmail.com>
2019-12-05 22:59:56 +0530

    Add some performance improvement in buffer creation

--- src/file.rs
+++ src/file.rs
@@ -3,22 +3,28 @@ use std::{
   io::{BufReader, Error, Read},
 };
 
+fn new_slice_with_capacity<T: Copy>(capacity: usize) -> Box<[T]> {
+  let mut vector = Vec::with_capacity(capacity);
+  unsafe { vector.set_len(capacity) };
+  vector.into_boxed_slice()
+}
+
 pub struct FileIterator {
   buf_reader: BufReader<File>,
-  buffer: Vec<u8>,
+  buffer: Box<[u8]>,
 }
 
 impl FileIterator {
   pub fn new(file_name: &str, buffer_size: usize) -> Result<Self, Error> {
     let file = File::open(file_name)?;
     let buf_reader = BufReader::new(file);
-    let buffer = vec![0u8; buffer_size];
+    let buffer = new_slice_with_capacity(buffer_size);
     Ok(FileIterator { buf_reader, buffer })
   }
 }
 
 impl Iterator for FileIterator {
-  type Item = Vec<u8>;
+  type Item = Box<[u8]>;
   fn next(&mut self) -> Option<Self::Item> {
     match self.buf_reader.read(&mut self.buffer) {
       Ok(nread) if nread > 0 => Some(self.buffer.clone()),
